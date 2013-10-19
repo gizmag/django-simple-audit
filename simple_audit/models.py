a4ad8d93d7db9f82e5262efe3d2f6c7128ea9e4d
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2013-10-18 21:32:32 -0300

    Revert "fix issue #4"
    This reverts commit 0afe55a92b7886fa65a0c4a75735ed7e55d4d198.

--- simple_audit/models.py
+++ simple_audit/models.py
@@ -5,7 +5,7 @@ import threading
 import uuid
 
 from django.db import models
-from .utils import User
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.utils.translation import ugettext_lazy as _
--- simple_audit/utils.py
+++
@@ -1,8 +0,0 @@
-import django
-
-# add suport to django >1.5 Custom user
-if django.VERSION >= (1, 5):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-else:
-    from django.contrib.auth.models import User
