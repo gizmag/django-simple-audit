28420ea74ddd42cc81f880a4cc826220a3ffd385
Justin Arulnathan <justin@gizmag.com>
2014-02-10 16:29:34 +1100

    only allow one of DJANGO_SIMPLE_AUDIT_M2M_FIELDS or DJANGO_SIMPLE_AUDIT_M2M_RELATIONS, never both

--- simple_audit/settings.py
+++ simple_audit/settings.py
@@ -1,6 +1,7 @@
 """Settings for django-simple-audit"""
 import logging
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 LOG = logging.getLogger(__name__)
 
@@ -11,3 +12,6 @@ DJANGO_SIMPLE_AUDIT_M2M_RELATIONS = getattr(settings, 'DJANGO_SIMPLE_AUDIT_M2M_R
 if not hasattr(settings, 'CACHES'):
     LOG.warning("no cache backend set in django! m2m auditing will be disabled")
     DJANGO_SIMPLE_AUDIT_M2M_FIELDS = False
+
+if DJANGO_SIMPLE_AUDIT_M2M_FIELDS and DJANGO_SIMPLE_AUDIT_M2M_RELATIONS:
+    raise ImproperlyConfigured("You cannot audit both M2M field changes and relations - choose one or the other")
