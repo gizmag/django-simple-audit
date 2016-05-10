7aabdd357d9cd6ccaed87b5e0c37eb1d3a117070
Tyson Clugg <tyson@clugg.net>
2016-05-10 13:46:28 +1000

    Fix install_requires allowing Django 1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ STATUS_PROD = 'Development Status :: 5 - Production/Stable'
 STATUS_BETA = 'Development Status :: 4 - Beta'
 STATUS_ALPHA = 'Development Status :: 3 - Alpha'
 
-version = '0.2.1'
+version = '0.2.2'
 README = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_description = open(README).read()
 setup(
@@ -30,5 +30,7 @@ setup(
     license='BSD',
     packages=find_packages('.', exclude=('testproject*',)),
     include_package_data=True,
-    install_requires=['Django>=1.8']
+    install_requires=[
+        'Django>=1.7',
+    ],
 )
